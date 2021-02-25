7770adb3826919da27e385a126e7aa5c8d48d340
Džiugas Praninskas <42064096+Dziugaske@users.noreply.github.com>
2021-02-25 22:54:46 +0200

    3 užduotis

---
+++ OOP paskaitos/Mokomoji 3/CentralProcessingUnit.hpp
@@ -0,0 +1,42 @@
+#ifndef CENTRAL_PROCESSING_UNIT
+#define CENTRAL_PROCESSING_UNIT
+
+
+class CentralProcessingUnit {
+    private:
+        string manufacturerName;
+        string modelName;
+        int cores;
+        int threads;
+        float voltage;
+        float tdp;
+        float baseClock;
+        float multiplier;
+        float coreClock;
+    public:
+        CentralProcessingUnit(string manufacturerName, string modelName);
+        ~CentralProcessingUnit();
+        void setCores(int cores);
+        int getCores();
+        void setThreads(int threads);
+        int getThreads();
+        void setVoltage(float voltage);
+        float getVoltage();
+        void setTdp(float tdp);
+        float getTdp();
+        void setBaseClock(float baseClock);
+        float getBaseClock();
+        void setMultiplier(float multiplier);
+        float getMultiplier();
+        void setCoreClock(float coreClock);
+        float getCoreClock();
+    private:
+        void calculateBaseClock();
+        void calculateMultiplier();
+        void calculateCoreClock();
+    public:
+        string toString();
+};
+
+
+#endif
