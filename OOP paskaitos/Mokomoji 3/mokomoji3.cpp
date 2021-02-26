02c53d1d28c4323876a56444970ce9db69353133
Džiugas Praninskas <42064096+Dziugaske@users.noreply.github.com>
2021-02-26 13:50:40 +0200

    UML update and testing in main()

--- OOP paskaitos/Mokomoji 3/mokomoji3.cpp
+++ OOP paskaitos/Mokomoji 3/mokomoji3.cpp
@@ -15,13 +15,28 @@ int main() {
         cpu.setTdp(450);
         cpu.setBaseClock(123.1);
         cpu.setMultiplier(13.4);
-        cout << cpu.toString();
+        cout << cpu.toString() << endl;
 
         vector<CentralProcessingUnit> cpuArray;
-        for (int i = 3000; i < 9000; i += 100) {
-            cpuArray.push_back(CentralProcessingUnit("Intel", to_string(i)));
+        for (int i = 1; i < 200; ++i) {
+            cpuArray.push_back(CentralProcessingUnit("Intel", to_string(i * 100)));
+            cpuArray[i - 1].setCores(i % 10 + 1);
+            cpuArray[i - 1].setVoltage(i % 2 + 0.5);
+            cpuArray[i - 1].setTdp(i * 5);
+            cpuArray[i - 1].setBaseClock(i * 5);
+            cpuArray[i - 1].setMultiplier(i % 49 + 1);
+            cout << cpuArray[i - 1].toString() << endl;
         }
-        cout << cpuArray[9].toString();
+        //Testing all the get methods:
+        cout << cpuArray[1].getCores() << endl;
+        cout << cpuArray[1].getThreads() << endl;
+        cout << cpuArray[1].getTdp() << endl;
+        cout << cpuArray[1].getVoltage() << endl;
+        cout << cpuArray[1].getBaseClock() << endl;
+        cout << cpuArray[1].getMultiplier() << endl;
+        cout << cpuArray[1].getCoreClock() << endl;
+        //Throws an exception:
+        cpuArray[5].setCores(-1);
     } catch (invalid_argument& e) {
         cout << e.what() << endl;
     } catch (...) {
