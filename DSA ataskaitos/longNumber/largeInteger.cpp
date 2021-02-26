e44b77e5dcd7e691daa41ce9f6384031a0d085fe
Džiugas Praninskas <42064096+Dziugaske@users.noreply.github.com>
2021-02-26 19:25:14 +0200

    formatting

--- DSA ataskaitos/longNumber/largeInteger.cpp
+++ DSA ataskaitos/longNumber/largeInteger.cpp
@@ -1,24 +1,23 @@
-#include <iostream>
-#include <fstream>
-#include <vector>
-#include <string>
-#include <sstream>
-#include <algorithm>
 #include "largeInteger.h"
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
 
 LargeInteger::LargeInteger(int size) {
     std::vector<int> temp(size, 0);
     digits = temp;
 }
-LargeInteger::LargeInteger(std::string initialString){
+LargeInteger::LargeInteger(std::string initialString) {
     setValue(initialString);
 }
 void LargeInteger::setValue(std::string initialString) {
-    for (int i = 0; i < initialString.length(); ++i){
-        if (initialString[i] >= '0' && initialString[i] <= '9'){
+    for (int i = 0; i < initialString.length(); ++i) {
+        if (initialString[i] >= '0' && initialString[i] <= '9') {
             digits.push_back(initialString[i] - 48);
-        }
-        else {
+        } else {
             throw 1;
         }
     }
@@ -30,7 +29,7 @@ LargeInteger LargeInteger::operator+(const LargeInteger &n2) {
         int remainder = 0;
         for (int i = 0; i < n2.digits.size(); ++i) {
             n.digits[i] = digits[i] + n2.digits[i] + remainder;
-            if (n.digits[i] / 10 == 1){
+            if (n.digits[i] / 10 == 1) {
                 n.digits[i] %= 10;
                 remainder = 1;
             } else {
@@ -39,7 +38,7 @@ LargeInteger LargeInteger::operator+(const LargeInteger &n2) {
         }
         for (int i = n2.digits.size(); i < digits.size(); ++i) {
             n.digits[i] = n.digits[i] + digits[i] + remainder;
-            if (n.digits[i] / 10 == 1){
+            if (n.digits[i] / 10 == 1) {
                 n.digits[i] %= 10;
                 remainder = 1;
             } else {
@@ -55,7 +54,7 @@ LargeInteger LargeInteger::operator+(const LargeInteger &n2) {
         int remainder = 0;
         for (int i = 0; i < digits.size(); ++i) {
             n.digits[i] = digits[i] + n2.digits[i] + remainder;
-            if (n.digits[i] / 10 == 1){
+            if (n.digits[i] / 10 == 1) {
                 n.digits[i] %= 10;
                 remainder = 1;
             } else {
@@ -64,7 +63,7 @@ LargeInteger LargeInteger::operator+(const LargeInteger &n2) {
         }
         for (int i = digits.size(); i < n2.digits.size(); ++i) {
             n.digits[i] = n.digits[i] + n2.digits[i] + remainder;
-            if (n.digits[i] / 10 == 1){
+            if (n.digits[i] / 10 == 1) {
                 n.digits[i] %= 10;
                 remainder = 1;
             } else {
@@ -77,10 +76,10 @@ LargeInteger LargeInteger::operator+(const LargeInteger &n2) {
         return n;
     }
 }
-std::string LargeInteger::toString(){
+std::string LargeInteger::toString() {
     std::ostringstream ss;
     std::reverse(digits.begin(), digits.end());
-    for (int i = 0; i < digits.size(); ++i){
+    for (int i = 0; i < digits.size(); ++i) {
         ss << digits[i];
     }
     return ss.str();
